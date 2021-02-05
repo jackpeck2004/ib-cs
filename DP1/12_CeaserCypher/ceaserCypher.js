a12c261c91fb9e1f144262cd40aa224cacf80b8d
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-02-05 12:48:29 +0100

    created ceaser cypher

---
+++ DP1/12_CeaserCypher/ceaserCypher.js
@@ -0,0 +1,54 @@
+const ALPHABET_STRING = "abcdefghijklmnopqrstuvwxyz";
+const ALPHABET = ALPHABET_STRING.split("");
+
+// return the shifted alphabet
+function createShiftedAlphabet(key, alphabet) {
+  const shiftedAlphabet = new Array(ALPHABET.length);
+
+  for (let i = 0; i < shiftedAlphabet.length; i = i + 1) {
+    let shiftedKey = i + key;
+    if (shiftedKey + 1 > alphabet.length) {
+      shiftedKey = shiftedKey - alphabet.length;
+    }
+
+    shiftedAlphabet[i] = alphabet[shiftedKey];
+  }
+
+  return shiftedAlphabet;
+}
+
+function encrypt(message, alphabet, shiftedAlphabet) {
+  const msgArr = message.split("");
+
+  for (let i = 0; i < msgArr.length; i = i + 1) {
+    msgArr[i] = shiftedAlphabet[alphabet.indexOf(msgArr[i])];
+  }
+
+  return msgArr.join("");
+}
+
+function encrypt(message, alphabet, shiftedAlphabet) {
+  const msgArr = message.split("");
+
+  for (let i = 0; i < msgArr.length; i = i + 1) {
+    msgArr[i] = shiftedAlphabet[alphabet.indexOf(msgArr[i])];
+  }
+
+  return msgArr.join("");
+}
+
+function decrypt(message, alphabet, shiftedAlphabet) {
+  const msgArr = message.split("");
+
+  for (let i = 0; i < msgArr.length; i = i + 1) {
+    msgArr[i] = alphabet[shiftedAlphabet.indexOf(msgArr[i])];
+  }
+
+  return msgArr.join("");
+}
+
+const shiftedAlphabet = createShiftedAlphabet(2, ALPHABET);
+
+const e = encrypt("hello from giacomo pasin", ALPHABET, shiftedAlphabet);
+console.log(e);
+console.log(decrypt(e, ALPHABET, shiftedAlphabet));
