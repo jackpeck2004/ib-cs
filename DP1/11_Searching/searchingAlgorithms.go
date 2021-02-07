678a33b51260acc9cf7ca4087666116b6ec1e1d5
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-02-07 14:50:19 +0100

    Sequential Search in go
    Created Sequential Search in GoLang and renamed the file from
    binarySearch.go to searchingAlgorithms.go since it contains
    more that one searching algorithm.

--- DP1/11_Searching/searchingAlgorithms.go
+++ DP1/11_Searching/searchingAlgorithms.go
@@ -22,8 +22,19 @@ func binarySearch(arr []int, n int) bool {
   return found
 }
 
+func sequentialSearch(arr []int, n int) bool {
+  for i := 0; i < len(arr); i++ {
+    if(arr[i] == n) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 func main() {
   var array = []int{1,3,5}
   n := 1
   fmt.Println(binarySearch(array, n))
+  fmt.Println(sequentialSearch(array, n))
 }
