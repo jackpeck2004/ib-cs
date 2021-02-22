9c198393a827d692ce3421926129e649547ac239
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-02-22 09:04:28 +0100

    moved methods to inside class

--- DP1/HL_08_BinaryTree/python/main.py
+++ DP1/HL_08_BinaryTree/python/main.py
@@ -6,28 +6,7 @@ _4 = Node(4, None, None)
 _5 = Node(5, None, None)
 root = Node(10, _4, _5)
 
-def print_pre_order(currentNode):
-  if currentNode is None:
-    return
+root.print_pre_order()
+root.print_post_order()
+root.print_in_order()
 
-  print(currentNode.key)
-  print_pre_order(currentNode.left)
-  print_pre_order(currentNode.right)
-
-def print_post_order(currentNode):
-  if currentNode is None:
-    return
-
-  print_pre_order(currentNode.left)
-  print_pre_order(currentNode.right)
-  print(currentNode.key)
-
-def print_in_order(currentNode):
-  if currentNode is None:
-    return
-
-  print_pre_order(currentNode.left)
-  print(currentNode.key)
-  print_pre_order(currentNode.right)
-
-print_pre_order(root)
--- DP1/HL_08_BinaryTree/python/node.py
+++ DP1/HL_08_BinaryTree/python/node.py
@@ -1,5 +1,40 @@
-class Node:
+class Node(object):
   def __init__(self, key, left, right):
     self.key = key
     self.left = left
     self.right = right
+
+  def __check_current_node(self, currentNode):
+    if currentNode == "ciao":
+      return self
+    return currentNode
+
+  def print_pre_order(self, currentNode="ciao"):
+    currentNode = self.__check_current_node(currentNode)
+
+    if currentNode is None:
+      return
+
+    print(currentNode.key)
+    self.print_pre_order(currentNode.left)
+    self.print_pre_order(currentNode.right)
+
+  def print_post_order(self, currentNode="ciao"):
+    currentNode = self.__check_current_node(currentNode)
+
+    if currentNode is None:
+      return
+
+    self.print_post_order(currentNode.left)
+    self.print_post_order(currentNode.right)
+    print(currentNode.key)
+
+  def print_in_order(self, currentNode="ciao"):
+    currentNode = self.__check_current_node(currentNode)
+
+    if currentNode is None:
+      return
+
+    self.print_in_order(currentNode.left)
+    print(currentNode.key)
+    self.print_in_order(currentNode.right)
