9743590620e2a31f72d6c1a8b2163b3d5a2e15f6
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-03-19 10:22:46 +0100

    began creating crawler

--- DP1/HL_09_Crawler/main.py
+++ DP1/HL_09_Crawler/main.py
@@ -1,7 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import networkx as nx
 
-def main():
-    return
+url_queue = ['http://h-farm.com']
+graph = nx.Graph()
 
-
-if __name__ == "__main__":
-    main()
+while not len(url_queue) == 0:
+    url = url_queue.pop()
+    parsed_url = urlparse(url)
+    base_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for tag in soup.find_all('a'):
+        href = tag['href']
+        parsed_href = urlparse(href)
+        if bool(parsed_href.netloc):
+            url_queue.append(href)
+        else:
+            url_queue.append(f'{base_url}{href}')
