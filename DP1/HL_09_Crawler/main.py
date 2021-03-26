993cb828edc2ae3274fc17da9bc07185640d7c95
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-03-26 10:54:51 +0100

    Continued web crawler

--- DP1/HL_09_Crawler/main.py
+++ DP1/HL_09_Crawler/main.py
@@ -2,20 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import networkx as nx
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from random import randint
 
 url_queue = ['http://h-farm.com']
-graph = nx.Graph()
+G = nx.Graph()
 
-while not len(url_queue) == 0:
+seed = randint(0, 100000)
+count = 20
+draw = True
+
+while not len(url_queue) == 0 and count > 0:
     url = url_queue.pop()
     parsed_url = urlparse(url)
     base_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    for tag in soup.find_all('a'):
-        href = tag['href']
-        parsed_href = urlparse(href)
-        if bool(parsed_href.netloc):
-            url_queue.append(href)
-        else:
-            url_queue.append(f'{base_url}{href}')
+    print(f'{count}:\t{base_url}')
+    count -= 1
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for tag in soup.find_all('a'):
+            href = tag['href']
+            parsed_href = urlparse(href)
+            new_url = href
+            if not bool(parsed_href.netloc):
+                new_url = f'{base_url}{href}'
+            if not new_url in G:
+                url_queue.append(new_url)
+            else:
+                print("already in graph")
+            G.add_edge(url, new_url)
+    except Exception as e:
+        print("Error:", e)
+
+layout = nx.spring_layout(G, scale=3)
+
+nx.draw(G, layout, with_labels=True)  # networkx draw()
+
+if draw:
+    plt.draw()  # pyplot draw()
+    plt.show()
+else:
+    plt.savefig('tmp.png')
