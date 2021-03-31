e91da7382ce4c7e552487d19706b226672c36d36
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-03-31 15:14:44 +0200

    Crawler: Added eigenvector centrality

--- DP1/HL_09_Crawler/main.py
+++ DP1/HL_09_Crawler/main.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 import networkx as nx
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 from random import randint
 
 url_queue = ['http://h-farm.com']
@@ -12,6 +13,20 @@ G = nx.Graph()
 seed = randint(0, 100000)
 count = 20
 draw = True
+
+
+def draw(G, pos, measures, measure_name):
+    nodes = nx.draw_networkx_nodes(G, pos, node_size=250, cmap=plt.cm.plasma,
+                                   node_color=list(measures.values()),
+                                   nodelist=measures.keys())
+    nodes.set_norm(mcolors.SymLogNorm(linthresh=0.01, linscale=1, base=10))
+    # labels = nx.draw_networkx_labels(G, pos)
+    edges = nx.draw_networkx_edges(G, pos)
+
+    plt.title(measure_name)
+    plt.colorbar(nodes)
+    plt.axis('off')
+    plt.show()
 
 while not len(url_queue) == 0 and count > 0:
     url = url_queue.pop()
@@ -38,6 +53,8 @@ while not len(url_queue) == 0 and count > 0:
 
 layout = nx.spring_layout(G, scale=3)
 
+"""
+
 nx.draw(G, layout, with_labels=True)  # networkx draw()
 
 if draw:
@@ -45,3 +62,6 @@ if draw:
     plt.show()
 else:
     plt.savefig('tmp.png')
+"""
+draw(G, layout, nx.eigenvector_centrality(G), 'Eigenvector Centrality')
+
