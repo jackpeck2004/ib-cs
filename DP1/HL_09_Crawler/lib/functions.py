86182bbc2a4275682e846a938fef1149df50a47b
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-04-07 11:17:51 +0200

    Feat: Saves and reads graphs as json

--- DP1/HL_09_Crawler/lib/functions.py
+++ DP1/HL_09_Crawler/lib/functions.py
@@ -1,6 +1,9 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+import json
+
 import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import networkx as nx
+from networkx.readwrite import json_graph
 
 
 def draw(graph, pos, measures, measure_name):
@@ -16,3 +19,14 @@ def draw(graph, pos, measures, measure_name):
     plt.axis('off')
     plt.show()
 
+
+def read_graph_from_file(file):
+    with open(file, "r") as f:
+        raw_graph = json.load(f)
+        return json_graph.node_link_graph(raw_graph)
+
+
+def write_graph_to_file(file, graph):
+    data = json_graph.node_link_data(graph)
+    with open(file, "w") as f:
+        json.dump(data, f, indent=4)
--- DP1/HL_09_Crawler/main.py
+++ DP1/HL_09_Crawler/main.py
@@ -1,38 +1,63 @@
+import json
+import os
+from urllib.parse import urlparse
+
+import networkx as nx
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-import networkx as nx
-from random import randint
+from networkx.readwrite import json_graph
+
 import lib.functions as fn
 
 url_queue = ['http://h-farm.com']
 G = nx.Graph()
 
 count = 20
+seed = count
 
-while not len(url_queue) == 0 and count > 0:
-    url = url_queue.pop()
-    parsed_url = urlparse(url)
-    base_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
-    print(f'{count}:\t{base_url}')
-    count -= 1
+filename = f"{urlparse(url_queue[0]).netloc}_{seed}.json"
+file_path = f"out/{filename}"
+
+# Check if a saved graph already exists
+if not os.path.exists(file_path):
+    # If not scrape the website and create graph
+    while not len(url_queue) == 0 and count > 0:
+        url = url_queue.pop()
+        parsed_url = urlparse(url)
+        base_url = f'{parsed_url.scheme}://{parsed_url.netloc}'
+        print(f'{count}:\t{base_url}')
+        count -= 1
+        try:
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+            for tag in soup.find_all('a'):
+                href = tag['href']
+                parsed_href = urlparse(href)
+                new_url = href
+                if not bool(parsed_href.netloc):
+                    new_url = f'{base_url}{href}'
+                if new_url not in G:
+                    url_queue.append(new_url)
+                else:
+                    print("already in graph")
+                G.add_edge(url, new_url)
+        except Exception as e:
+            print("Error:", e)
+
+    # Write the graph to a file
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for tag in soup.find_all('a'):
-            href = tag['href']
-            parsed_href = urlparse(href)
-            new_url = href
-            if not bool(parsed_href.netloc):
-                new_url = f'{base_url}{href}'
-            if not new_url in G:
-                url_queue.append(new_url)
-            else:
-                print("already in graph")
-            G.add_edge(url, new_url)
+        fn.write_graph_to_file(file_path, G)
     except Exception as e:
-        print("Error:", e)
+        print("Error when writing file:", e)
 
+# Check if graph is empty
+if nx.is_empty(G):
+    # Attempt to read Graph from past saved graph from json file
+    try:
+        G = fn.read_graph_from_file(file_path)
+    except Exception as e:
+        print("Error when reading file:", e)
+
+# Draw graph
 layout = nx.spring_layout(G, scale=3)
-
 fn.draw(G, layout, nx.eigenvector_centrality(G), 'Eigenvector Centrality')
