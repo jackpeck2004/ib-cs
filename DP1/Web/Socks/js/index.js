404380ed1926b95fa074755afb21e4ce8f4eadd7
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-04-27 11:59:52 +0200

    Socks: completed

--- DP1/Web/Socks/js/index.js
+++ DP1/Web/Socks/js/index.js
@@ -21,6 +21,48 @@ function handleFormSubmit(event) {
   sockInput.value = "";
 }
 
-const form = document.querySelector("#sock-form");
+function maxMinInArray(array) {
+  let max = array[0];
+  let min = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i];
+    }
+    if (min > array[i]) {
+      min = array[i];
+    }
+  }
 
+  return [max, min];
+}
+
+function handleCountBtnClick() {
+  const [max, min] = maxMinInArray(socks);
+  const freq = [];
+
+  for (let i = min; i <= max; i++) {
+    freq.push(0);
+  }
+
+  for (let i = 0; i < socks.length; i++) {
+    const sock = socks[i];
+    freq[sock - min]++;
+  }
+
+  let pairs = 0;
+
+  for (let i = 0; i < freq.length; i = i + 1) {
+    let f = freq[i];
+    if (f % 2 == 1) {
+      f--;
+    }
+    pairs += f / 2;
+  }
+  alert(pairs);
+}
+
+const form = document.querySelector("#sock-form");
 form.addEventListener("submit", handleFormSubmit);
+
+const countBtn = document.querySelector("#count-button");
+countBtn.addEventListener("click", handleCountBtnClick);
