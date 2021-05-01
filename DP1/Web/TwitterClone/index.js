8059236cb3fc88b31931dc2f00d6c5190409ed51
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-05-01 20:38:37 +0200

    TwitterClone: Continued development of front page

--- DP1/Web/TwitterClone/index.js
+++ DP1/Web/TwitterClone/index.js
@@ -6,11 +6,30 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(expressLayouts);
-app.set('layout', './partials/layout');
+app.use(express.static(`${__dirname}/public`));
+app.set('layout', './layouts/layout');
 app.set('view engine', 'ejs');
 
+// const links = [
+//   {
+//     value: 'Home',
+//     path: '/',
+//   },
+// ];
+
 app.get('/', (req, res) => {
-  res.render('pages/home');
+  res.render('pages/home', {
+    title: 'Home',
+  });
 });
+
+if (process.env.NODE_ENV === 'development') {
+  logger.info('Running in development mode');
+  app.get('/:route', (req, res) => {
+    res.render(`pages/${req.params.route}`, {
+      title: req.params.route,
+    });
+  });
+}
 
 app.listen(port, () => logger.info(`Server listening on port ${port}...`));
