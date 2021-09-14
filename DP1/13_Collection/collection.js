ecb13f2174d9fb724c756da61088977b098f6a78
Giacomo Pasin <giacomo.pasin@gmail.com>
2021-09-14 11:32:31 +0200

    implemented hasNext and next instance methods

--- DP1/13_Collection/collection.js
+++ DP1/13_Collection/collection.js
@@ -1,16 +1,24 @@
 class Collection {
     constructor() {
+        this._cursor = 0;
+        this._size = 6;
+        this._values = [1, 2, 3, 4, 5, 6];
     }
 
     // bool
     hasNext() {
-        console.log("hasnext")
+        if (this._cursor < this._size) {
+            return true;
+        } 
 
+        return false;
     }
 
     // value
     next() {
-
+        const value = this._values[this._cursor];
+        this._cursor += 1;
+        return value;
     }
 }
 
